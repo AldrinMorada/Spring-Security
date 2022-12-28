80eb2f3d8ca4a2cbec27dca671a0eb2462e16e5d
Aldrin Morada <moradaaldrin123@gmail.com>
2022-12-28 11:59:58 +0800

    added table for users

--- src/main/java/com/laundryManagement/demo/controller/AdminController.java
+++ src/main/java/com/laundryManagement/demo/controller/AdminController.java
@@ -1,12 +1,21 @@
 package com.laundryManagement.demo.controller;
 
+import com.laundryManagement.demo.model.User;
+import com.laundryManagement.demo.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/admin")
 public class AdminController {
+
+    @Autowired
+    UserRepository userRepo;
 
     @GetMapping("/")
     public String getIndex() {
@@ -14,7 +23,9 @@ public class AdminController {
     }
 
     @GetMapping("/users")
-    public String getUsers() {
+    public String getUsers(Model m) {
+        List<User> users = userRepo.findAll();
+        m.addAttribute("users", users);
         return "admin/user";
     }
 
